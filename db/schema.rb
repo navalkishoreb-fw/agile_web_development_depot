59bed610fa2c3975a2baf742594fa1a0184ee621
Naval Kishore Barthwal <naval.kishorebarthwal@freshworks.com>
2024-07-26 09:29:16 +0530

    Iteration J1: Adding Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_25_162129) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_26_033737) do
   create_table "carts", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_25_162129) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
