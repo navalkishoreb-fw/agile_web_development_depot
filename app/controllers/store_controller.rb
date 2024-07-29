550bd91845c47eec6d0fc8ad106e75fc7d27835c
Naval Kishore Barthwal <naval.kishorebarthwal@freshworks.com>
2024-07-29 08:19:28 +0530

    add redis cache in store_controller.rb

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -7,7 +7,16 @@ class StoreController < ApplicationController
     if params[:set_locale]
       redirect_to store_index_url(locale: params[:set_locale])
     else
-      @products = Product.order(:title)
+      @products = fetch_products
+    end
+  end
+
+  def fetch_products
+    Rails.cache.fetch("products", expires_in: 5.minutes) do
+      Rails.logger.info("Cache miss for products")
+      Product.order(:title).to_a
+    end.tap do |products|
+      Rails.logger.info("Cache hit for products") unless products.nil?
     end
   end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,8 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+    # config.cache_store = :null_store
+    config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] || 'redis://localhost:6379/0', namespace: 'cache', compress: true }
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
